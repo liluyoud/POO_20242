1ce55ad9117431d56ed2db8ba2d033e89c49a300
Liluyoud Cury de Lacerda <liluyoud@hotmail.com>
2024-10-30 20:15:10 -0400

    Aula 8 - modelo 2

---
+++ Aula08/Modelo2.cs
@@ -0,0 +1,41 @@
+﻿// um banco tem 3 tipos de contas bancárias: 
+//  . corrente, poupança e investimentos
+// tb ele trabalha com dois tipos de clientes: 
+//  . pessoa física e pessoa jurídica
+// elabore um modelo que controle as transações bancárias 
+// de cada cliente
+
+namespace modelo2;
+
+public class Banco {
+    public List<ContaBancaria>? Contas { get; set; }
+    public List<Cliente>? Clientes { get; set; }
+}
+
+public class ContaBancaria {
+    public Cliente? Cliente { get; set; }
+    public List<Transacao>? Transacoes { get; set; }
+    public TipoConta? Tipo {  get; set; }
+}
+
+public class Transacao {
+    public ContaBancaria? Conta { get; set; }
+}
+
+public class Cliente {
+    public TipoCliente? Tipo { get; set; }
+    public List<ContaBancaria>? Contas { get; set; }
+}
+
+public enum TipoConta
+{
+    Corrente,
+    Poupanca,
+    Investimento
+}
+
+public enum TipoCliente
+{
+    Pessoa,
+    Empresa
+}
--- Aula08/Program.cs
+++ Aula08/Program.cs
@@ -1,36 +1,7 @@
-﻿// um banco tem 3 tipos de contas bancárias: 
-//  . corrente, poupança e investimentos
-// tb ele trabalha com dois tipos de clientes: 
-//  . pessoa física e pessoa jurídica
-// elabore um modelo que controle as transações bancárias 
-// de cada cliente
+﻿using modelo2;
 
-namespace modelo1;
+ContaBancaria conta = new();
+conta.Tipo = TipoConta.Corrente;
 
-public class Banco {
-    public List<ContaBancaria>? Contas { get; set; }
-    public List<Cliente>? Clientes { get; set; }
-}
-
-public class ContaBancaria {
-    public Cliente? Cliente { get; set; }
-    public List<Transacao>? Transacoes { get; set; }
-}
-
-public class ContaCorrente : ContaBancaria { }
-
-public class ContaPoupanca : ContaBancaria { }
-
-public class ContaInvestimento : ContaBancaria { }
-
-public class PessoaFisica : Cliente { }
-
-public class PessoaJuridica : Cliente { }
-
-public class Transacao {
-    public ContaBancaria? Conta { get; set; }
-}
-
-public class Cliente {
-    public List<ContaBancaria>? Contas { get; set; }
-}
+Cliente cliente = new();
+cliente.Tipo = TipoCliente.Pessoa;
